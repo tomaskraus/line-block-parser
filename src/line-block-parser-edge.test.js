06e2402a46034cbf900e95d7bbb43964ae4da879
Tomas Kraus <tomas.kraus.dev@gmail.com>
2021-04-05 14:43:24 +0200

    add edge cases test

---
+++ src/line-block-parser-edge.test.js
@@ -0,0 +1,65 @@
+const fu = require("./func-utils");
+const { Parser, Tags } = require("./line-block-parser");
+
+const parserFlat = Parser.create(Tags.js_block.start, Tags.js_block.end, false);
+
+const parserGrouped = Parser.create(
+  Tags.js_block.start,
+  Tags.js_block.end,
+  true
+);
+
+//-------------------------------------------
+
+const testLines = (lines, label = "") => {
+  fu.log(
+    `-- lines (${label}) ------------------------------------------------------------------------`
+  );
+  fu.log("", lines);
+  //
+  fu.log("-- parser flat (FLAT mode, no callback) ---------");
+  fu.log("test result: ", parserFlat.parseLines(lines));
+
+  fu.log("-- parser grouped (GROUPED mode, no callback) ---------");
+
+  const parsedData = parserGrouped.parseLines(lines).data;
+
+  fu.log("test result data: ", parsedData);
+  // fu.log("data[0]:", parserGrouped.parseLines(lines).data[0]);
+  // fu.log("data[1]:", parserGrouped.parseLines(lines).data[1]);
+};
+//------------------------------------------------------------------------------------------------
+
+const emptyLines = [];
+testLines(emptyLines, "emptyLines");
+
+//
+
+const noCommentLines = [
+  "https://en.wikipedia.org/wiki/Mary_Had_a_Little_Lamb",
+  "Mary had a little lamb,",
+  "   The lamb was sure to go ;",
+  "He followed her to school one day-",
+  "--   That was against the rule,",
+  "",
+  "It made the children laugh and play,",
+  "   To see a lamb at school.",
+];
+testLines(noCommentLines, "no comment");
+
+//
+
+const wholeComment = [
+  // "ee",
+  "  /* ",
+  "https://en.wikipedia.org/wiki/Mary_Had_a_Little_Lamb",
+  "Mary had a little lamb,",
+  "   The lamb was sure to go ;",
+  "He followed her to school one day-",
+  "--   That was against the rule,",
+  "",
+  "It made the children laugh and play,",
+  "   To see a lamb at school.",
+  "   */    ",
+];
+testLines(wholeComment, "whole comment");
