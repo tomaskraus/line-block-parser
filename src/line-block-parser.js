aac058339c0dd23ffb4fa020622cd48de1e6173d
Tomas Kraus <tomas.kraus.dev@gmail.com>
2021-04-06 18:51:50 +0200

    universal accumulator is working

--- src/line-block-parser.js
+++ src/line-block-parser.js
@@ -125,13 +125,13 @@ const createPairParserEngine = (accum) => (lc) => {
   // fu.log("lc: ", lc);
   if (pState.beginBlockLineNum === NO_BLOCK_BEGIN) {
     if (lc.line.type === LEXER.START_TAG) {
-      //pState2.beginNotBlockLineNum = NO_BLOCK_BEGIN;
       let lc2 = accum.flush(null, lc);
       //fu.log("START TAG");
       let pState2 = lc2[LC.PARSER];
       pState2.lineType = lc2.line.type;
 
       pState2.beginBlockLineNum = lc2[LC.LINE_NUMBER] + 1;
+      pState2.beginNotBlockLineNum = NO_BLOCK_BEGIN;
       pState2.startTagLine = lc2.line.data;
       pState2.endTagLine = null;
       pState2.state = P_STATE.IN_BLOCK;
@@ -150,7 +150,6 @@ const createPairParserEngine = (accum) => (lc) => {
       pState.endTagLine = lc.line.data;
 
       lc = accum.flush(lc.line, lc);
-      //fu.log("END lc: ", lc);
 
       lc[LC.PARSER].beginBlockLineNum = NO_BLOCK_BEGIN;
       lc[LC.PARSER].beginNotBlockLineNum = lc[LC.LINE_NUMBER] + 1;
@@ -178,6 +177,10 @@ const getAcc = (lineContext) => lineContext[LC.ACCUM].data;
 const clearAcc = (lineContext) =>
   fu.setProp(LC.ACCUM, initialAccumState(), lineContext);
 
+const overAcc = fu.curry2((fn, lineContext) =>
+  fu.overProp(LC.ACCUM, fn, lineContext)
+);
+
 //flushAccum :: (a -> b) -> a -> lineContext -> lineContext
 const flushAccum = (resultCallback, data, lineContext) => {
   const cRes = resultCallback(data);
@@ -192,12 +195,7 @@ const flushAccum = (resultCallback, data, lineContext) => {
 
 //----------------------------------------------------------------------------------------
 
-const createAccum = (groupedFlag, resultCallback) =>
-  groupedFlag === true
-    ? createGroupedAccum(resultCallback)
-    : createFlatAccum(resultCallback);
-
-//----------------------------------------------------------------------------------------
+const plainParserDecorator = (data, _) => data.data;
 
 const infoParserDecorator = (data, lineContext) => ({
   lineNumber: lineContext[LC.LINE_NUMBER],
@@ -205,31 +203,6 @@ const infoParserDecorator = (data, lineContext) => ({
   state: lineContext[LC.PARSER].state,
   data,
 });
-
-const createFlatAccum = (resultCallback) => {
-  const accObj = {};
-
-  accObj.flush = (dataToFlush, lineContext) => {
-    if (dataToFlush === null) {
-      return lineContext;
-    }
-    return flushAccum(
-      resultCallback,
-      infoParserDecorator(dataToFlush.data, lineContext),
-      lineContext
-    );
-  };
-
-  accObj.append = (newValue, lineContext) => {
-    return accObj.flush(newValue, lineContext);
-  };
-
-  accObj.start = accObj.append;
-
-  return accObj;
-};
-
-//----------------------------------------------------------------------------------------
 
 const groupedParserDecorator = (AccumulatorData, lineContext) => ({
   state: lineContext[LC.PARSER].state,
@@ -242,41 +215,51 @@ const groupedParserDecorator = (AccumulatorData, lineContext) => ({
   data: AccumulatorData,
 });
 
-const plainParserDecorator = (data, _) => data.data;
+//
 
 const isValidToFlush = (lineContext) => {
-  if (
-    lineContext[LC.PARSER].state === P_STATE.IN_BLOCK &&
-    lineContext[LC.PARSER].beginBlockLineNum === NO_BLOCK_BEGIN
-  ) {
-    return false;
-  }
-  return lineContext[LC.PARSER].state != P_STATE.INIT;
+  return lineContext[LC.ACCUM].state === "ready";
 };
+//----------------------------------------------------------------------------------------
 
-const createGroupedAccum = (resultCallback) => {
+const createAccumulator = (groupedFlag, resultCallback) => {
   const accObj = {};
 
-  accObj.start = (_, lineContext) => lineContext;
+  accObj.flush = (dataToFlush, lineContext) => {
+    if (groupedFlag) {
+      return isValidToFlush(lineContext)
+        ? flushAccum(
+            resultCallback,
+            groupedParserDecorator(getAcc(lineContext), lineContext),
+            lineContext
+          )
+        : lineContext;
+    }
+    return dataToFlush === null
+      ? lineContext
+      : flushAccum(
+          resultCallback,
+          infoParserDecorator(dataToFlush.data, lineContext),
+          lineContext
+        );
+  };
 
   accObj.append = (data, lineContext) =>
-    fu.overProp(
-      LC.ACCUM,
-      (aObj) => ({
-        state: aObj.state,
-        data: [...aObj.data, plainParserDecorator(data, lineContext)],
-      }),
-      lineContext
-    );
-
-  accObj.flush = (_, lineContext) =>
-    isValidToFlush(lineContext)
-      ? flushAccum(
-          resultCallback,
-          groupedParserDecorator(getAcc(lineContext), lineContext),
+    groupedFlag === true
+      ? fu.overProp(
+          LC.ACCUM,
+          (aObj) => ({
+            state: "ready",
+            data: [...aObj.data, plainParserDecorator(data, lineContext)],
+          }),
           lineContext
         )
-      : lineContext;
+      : accObj.flush(data, lineContext);
+
+  accObj.start = (data, lineContext) =>
+    groupedFlag === true
+      ? overAcc((_) => ({ data: [], state: "ready" }), lineContext)
+      : accObj.flush(data, lineContext);
 
   return accObj;
 };
@@ -299,7 +282,7 @@ class Parser {
     )(initialLineContext());
 
   constructor(startTagRegExp, endTagRegExp, isGrouped, resultCallback) {
-    this.accum = createAccum(isGrouped, resultCallback);
+    this.accum = createAccumulator(isGrouped, resultCallback);
     this.lexer = createLexer(startTagRegExp, endTagRegExp);
     this.parserEngine = createPairParserEngine(this.accum);
     this.reducer = createReducer(this.lexer, this.parserEngine);
