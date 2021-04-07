53a87412de7d9f96ccad59d0eb79817dfe61d665
Tomas Kraus <tomas.kraus.dev@gmail.com>
2021-04-07 21:54:45 +0200

    make test ok result able to be versioned

--- test/_save_output.sh
+++ test/_save_output.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-node line-block-parser-pair.test.js > ../.temp/test_out_orig.txt
-node line-block-parser-pair.edge.test.js >> ../.temp/test_out_orig.txt
+node line-block-parser-pair.test.js > test_out_orig.txt
+node line-block-parser-pair.edge.test.js >> test_out_orig.txt
