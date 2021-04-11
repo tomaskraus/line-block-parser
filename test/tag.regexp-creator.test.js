ced0744862092f50e89581f16711055055c8df9f
Tomas Kraus <tomas.kraus.dev@gmail.com>
2021-04-11 17:56:12 +0200

    add first meaningful unit tests

--- test/tag.regexp-creator.test.js
+++ test/tag.regexp-creator.test.js
@@ -1,5 +1,15 @@
-const sum = require("../src/tag-regexp-creator.js");
+const { Tag } = require("../src/tag-regexp-creator.js");
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+test("empty Tag recognizes empty line", () => {
+  const tg = new Tag("");
+  expect(tg.regexp().test("")).toBe(true);
+});
+
+test("empty Tag recognizes white-chars line", () => {
+  const tg = new Tag("");
+  expect(tg.regexp().test(" ")).toBe(true);
+  expect(tg.regexp().test("  ")).toBe(true);
+  expect(tg.regexp().test("\t")).toBe(true);
+  expect(tg.regexp().test("  \t")).toBe(true);
+  expect(tg.regexp().test("  \t ")).toBe(true);
 });
